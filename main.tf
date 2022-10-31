9e8da6f9fdf2d79ce0c9451da76428150b628344
Mariia Mohylnytska <aleatoricmbnt@gmail.com>
2022-10-31 21:01:18 +0200

    test

--- main.tf
+++ main.tf
@@ -1,27 +1,4 @@
 #comment
-resource "random_pet" "resource" {
-  count = 1
-  keepers = {
-    string = var.var-var-var
-    string2 = var.var-var
-  }
-  length = var.len
-}
-
-variable "var-var-var" {
-  sensitive = true
-  default = "string"
-}
-
-variable "var-var" {
-  sensitive = false
-  default = "string"
-}
-
-variable "len" {
-  sensitive = true
-  default = 1234
-}
 
 variable "listssss" {
   type = list
