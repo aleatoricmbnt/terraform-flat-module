787c673bf51d4bc7077ad6e4df7dad53f1c3fd85
Mariia Mohylnytska <aleatoricmbnt@gmail.com>
2022-10-31 13:36:55 +0200

    test

--- main.tf
+++ main.tf
@@ -2,20 +2,16 @@
 resource "random_pet" "resource" {
   count = 1
   keepers = {
-    timestamp = timestamp()
-    number = var.user_number
     string = var.var-var-var
   }
   length = var.len
 }
 
-variable "user_number" {
-  type = number
-  description = "keeper var 1/22"
-}
+
 
 variable "var-var-var" {
-  default = "ya masha 123"
+  sensitive = true
+  default = "string"
 }
 
 variable "len" {
