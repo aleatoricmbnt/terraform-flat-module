99a7093173436ef7cbff0de51bc6d33df1b593c4
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-08-22 19:25:29 +0300

    fixed plan

--- main.tf
+++ main.tf
@@ -1,4 +1,24 @@
-module "referencing" {
-  source = "./pet_tbd"
+resource "random_pet" "keeped_by_time" {
+  keepers = {
+    time = timestamp()
+    var = var.trigger
+  }
 }
 
+data "null_data_source" "some_pairs" {
+  inputs = {
+    single-key = "single value"
+  }
+}
+
+variable "rigger" {
+  
+}
+
+output "random" {
+  value = data.null_data_source.some_pairs.random
+}
+
+output "value" {
+  value = data.null_data_source.some_pairs["single-key"]
+}
