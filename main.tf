3c0e2083d82e94a4a202b080d4f038bf9208bca7
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-10-17 15:48:00 +0300

    fixed misconfig for terraform-nested-module top-level it5

--- main.tf
+++ main.tf
@@ -18,14 +18,14 @@ resource "random_string" "strings" {
 
 data "null_data_source" "some_values" {
   inputs = {
-    first_pair = toset(
+    first_pair = toset([
       random_string.strings[0].id,
       random_string.strings[1].id,
-    )
-    second_pair = toset(
+    ])
+    second_pair = toset([
       random_string.strings[2].id,
       random_string.strings[3].id,
-    )
+    ])
   }
 }
 
